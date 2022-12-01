4bfb4fb0cdf113db698202f33c253f29b90f033a
Ibrahim-elyazidi <ibrahim.el-yazidi@lyceedecachan-sti.fr>
2022-12-01 12:32:38 +0100

    Controller

---
+++ src/Controller/ConfirmResaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConfirmResaController extends AbstractController
+{
+    #[Route('/confirm/resa', name: 'app_confirm_resa')]
+    public function index(): Response
+    {
+        return $this->render('confirm_resa/index.html.twig', [
+            'controller_name' => 'ConfirmResaController',
+        ]);
+    }
+}

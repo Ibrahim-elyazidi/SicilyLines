e1edd9ad38c24fcfc1ceee10d8c123371164bb11
Ibrahim-elyazidi <ibrahim.el-yazidi@lyceedecachan-sti.fr>
2023-03-30 13:05:06 +0200

    modification liaison

--- src/Form/LiaisonType.php
+++ src/Form/LiaisonType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Liaison;
+use App\Entity\Port;
+use App\Entity\Secteur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class LiaisonType extends AbstractType
 {
@@ -13,9 +16,20 @@ class LiaisonType extends AbstractType
     {
         $builder
             ->add('duree')
-            ->add('port_depart')
-            ->add('port_arrive')
-            ->add('secteur')
+            ->add('port_depart', EntityType::class, [
+                'class'=> Port::class,
+                'choice_label'=> 'nom',
+            ])
+            ->add('port_arrive', EntityType::class, [
+                'class'=> Port::class,
+                'choice_label'=> 'nom',
+
+            ])
+            ->add('secteur', EntityType::class, [
+                'class'=> Secteur::class,
+                'choice_label'=> 'libelle'
+                
+            ])
         ;
     }
 
